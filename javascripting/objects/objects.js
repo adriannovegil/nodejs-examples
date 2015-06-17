6dfdd751c2b91b56fafdabca8d7f9a451cc3b3c2
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 15:40:23 +0200

    Añadido nuevo ejemplo de uso de NodeJS donde se ilustra la definición de objetos.

---
+++ javascripting/objects/objects.js
@@ -0,0 +1,8 @@
+// Definimos el objeto pizza con tres atributos e inicializamos estos atributos.
+var pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+// Mostramos por pantalla el contenido del objeto pizza.
+console.log(pizza);
