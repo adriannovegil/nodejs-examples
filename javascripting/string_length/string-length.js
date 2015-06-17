b9cabc65487554303de835b3cbf2824d0703e9f6
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 12:58:43 +0200

    Añadido nuevo ejemplo de uso de NodeJS. Uso de Strings.

---
+++ javascripting/string_length/string-length.js
@@ -0,0 +1,5 @@
+// Declaramos y definimos un valor para la variable example.
+var exampleString = 'example string';
+// Mostramos por pantalla el valor de la variable definida.
+console.log(exampleString.length);
+
