44c2425ea76fa062c99a8af6bc1baa7a544f217e
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 13:05:20 +0200

    Uso de la función replace para la manipulación de cadenas de caracteres.

---
+++ javascripting/revising_strings/revising-strings.js
@@ -0,0 +1,8 @@
+// Declaramos y definimos un valor para la variable example.
+var pizza = 'pizza is alright';
+// Reeemplazamos la cadena alright por wonderful y el resultado del reemplazo lo
+// asignamos de nuevo a la variable pizza.
+pizza = pizza.replace('alright', 'wonderful');
+// Mostramos por pantalla el valor de la variable definida.
+console.log(pizza);
+
