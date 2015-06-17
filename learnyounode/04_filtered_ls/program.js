6b0373545dbeae2637bf1bfc13e2ee27b23e4eca
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 17:31:21 +0200

    Nuevo ejemplo de uso de nodeJS. En este caso se ha implementado un comando estilo ls con filtrado por extensión.

---
+++ learnyounode/04_filtered_ls/program.js
@@ -0,0 +1,27 @@
+// Cargamos el módulo que contiene las funciones necesarias para la entrada/salida.
+var fs = require('fs');
+// Cargamos el módulo ya que contiene funciones necesarias para la gestión de los
+// paths, como por ejemplo, extraer la extensión a partir del path completo de un
+// fichero.
+var path = require('path');
+// Si el usuario ha especificado una extensión, la procesamos.
+if (process.argv.length >= 3) {
+    var extension = '.' + process.argv[3];
+}
+// Leemos el fichero. El path al mismo se lo pasamos al programa como parametro
+// del programa.
+// La función readFileSync retorna un buffer que contiene el fichero.
+fs.readdir(process.argv[2], function (err, list) {
+    // Recorremos la lista de ficheros.
+    for (var i = 0; i < list.length; i++) {
+        // Recuperamos el path al fichero actual.
+        var currentFile = list[i];
+        // Extraemos la extensión del fichero actual.
+        var currentExtension = path.extname(currentFile);
+        // verificamos que coinciden las extensiones.
+        if (currentExtension === extension) {
+            // Mostramos por pantalla el fichero actual que estamos procesando.
+            console.log(currentFile);
+        }
+    }
+});
