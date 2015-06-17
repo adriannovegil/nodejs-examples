279601b777848469766ea4a182cece63fb6b749a
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 13:38:17 +0200

    Nuevo ejemplo de uso de NodeJS. se muestra la definición de arrays.

---
+++ javascripting/arrays/arrays.js
@@ -0,0 +1,4 @@
+// Definimos el array y lo inicializamos con valores.
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+// Mostramos por pantalla el valor de la variable definida.
+console.log(pizzaToppings);
