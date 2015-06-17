916128623346b0bc98f8259c887fcad37bfd7d90
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 13:14:52 +0200

    Añadido nuevo ejemplo de uso de NodeJS. Manipulación de variables numéricas. Redondeo.

---
+++ javascripting/roundings_numbers/rounding-numbers.js
@@ -0,0 +1,7 @@
+// Declaramos y definimos un valor para la variable example.
+var roundUp = 1.5;
+// Definimos una segunda variable a la que le asignamos el valor resultado de
+// redondear el valor de la primera variable.
+var rounded = Math.round(roundUp);
+// Mostramos por pantalla el valor de la variable definida.
+console.log(rounded);
