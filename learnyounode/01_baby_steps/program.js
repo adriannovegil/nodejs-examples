215bdfa17c980eb1474d9611c833b8a0a3d487d2
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 16:39:49 +0200

    Nuevo ejemplo de NodeJS. Se muestra como procesar los argumentos del programa.

---
+++ learnyounode/01_baby_steps/program.js
@@ -0,0 +1,12 @@
+// Mostramos el mensaje "HELLO WORLD" por la consola del servidor.
+var result = 0;
+// Recorremos la lista de argumentos. Como los dos primeros no los hemos especificado 
+// nosotros si no que es el comando y el programa que queremos ejecutar, empezamos
+// a iterar en el indice 2.
+for (var i = 2; i < process.argv.length; i++) {
+    // Parseamos los valores pasados como parámetro a número y los sumamos a la
+    // variable result.
+    result += Number(process.argv[i]);
+}
+// Mostramos por pantalla el resultado de sumar los valores pasados como parametro.
+console.log(result);
