667fbdc39cba55dfd0b2d38b1ebc4bbfb739473e
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 15:54:54 +0200

    Ejemplo de definición de funciones y argumentos.

---
+++ javascripting/function_arguments/function-arguments.js
@@ -0,0 +1,15 @@
+/**
+ * Función que opera sobre tres valores de tipo numérico que le pasamos como 
+ * parametros.
+ * @param {type} first Primer valor.
+ * @param {type} second Segundo valor.
+ * @param {type} third Tercer valor.
+ * @returns {unresolved} Resultado de operar los parametros de la función.
+ */
+function math(first, second, third) {
+    // Retornamos el resultado obtenido de operar.
+    return (second * third) + first;
+
+}
+// Mostramos por consola el resultado de ejecutar la función.
+console.log(math(53, 61, 67));
