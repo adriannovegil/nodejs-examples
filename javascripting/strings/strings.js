8e616df546b8f2b38c8aca62fdfc6be462f7cbcb
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 12:55:53 +0200

    Añadido nuevo ejemplo de uso de NodeJS. Definición básica de Strings.

---
+++ javascripting/strings/strings.js
@@ -0,0 +1,5 @@
+// Declaramos y definimos un valor para la variable example.
+var someString = 'this is a string';
+// Mostramos por pantalla el valor de la variable definida.
+console.log(someString);
+
