2a266deb6d48b746845a6711d3f440982984c4be
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 13:32:55 +0200

    Añadido nuevo ejemplo de uso de NodeJS. Muestra de uso del bloque for.

---
+++ javascripting/for_loop/for-loop.js
@@ -0,0 +1,10 @@
+// Declaramos y definimos un valor para la variable example.
+var total = 0;
+var limit = 10;
+// Iteramos 10 veces
+for (var i = 0; i < limit; i++) {
+    // Le sumamos a la variable total el valor de i en la iteración actual.
+    total += i;
+}
+// Mostramos por pantalla el valor de la variable definida.
+console.log(total);
