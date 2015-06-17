7c5c0bd271ec193d4500f12a955e28d9d3e9c211
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-06-17 16:02:32 +0200

    Dummy para testear el ambito en la definición de funciones y variables.

---
+++ javascripting/scope/scope.js
@@ -0,0 +1,18 @@
+// Definimos las variables originales y les asignamos valor.
+var a = 1, b = 2, c = 3;
+// Funciones anidadas para testear con el scope de JavaScript.
+(function firstFunction() {
+    var b = 5, c = 6;
+    (function secondFunction() {
+        var b = 8;
+        // a: 1, b: 8, c: 6
+        console.log("a: " + a + ", b: " + b + ",c: " + c);
+        (function thirdFunction() {
+            var a = 7, c = 9;
+            (function fourthFunction() {
+                var a = 1, c = 8;
+
+            })();
+        })();
+    })();
+})();
