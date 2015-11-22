673c3fbbd4c14684d0b87f4354772138bb22592d
Adrian Novegil <adrian.novegil@adesis.com>
2015-11-21 20:21:56 -0600

    Modificación menor en un ejemplo de del paquete de javascript y reorganización de los paquetes.

--- 00_javascripting/18_scope/scope.js
+++ 00_javascripting/18_scope/scope.js
@@ -6,7 +6,7 @@ var a = 1, b = 2, c = 3;
     (function secondFunction() {
         var b = 8;
         // a: 1, b: 8, c: 6
-        console.log("a: " + a + ", b: " + b + ",c: " + c);
+        console.log("a: " + a + ", b: " + b + ", c: " + c);
         (function thirdFunction() {
             var a = 7, c = 9;
             (function fourthFunction() {
