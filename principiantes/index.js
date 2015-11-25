a038fd57dd327ac9cb295a9ae0e5c2e9d81993f2
Adrian Novegil <adrian.novegil@adesis.com>
2015-11-24 22:10:36 -0600

    Modularizamos la aplicación.

---
+++ principiantes/index.js
@@ -0,0 +1,5 @@
+// Importamos el módulo que hemos sdefinido.
+var server = require("./server");
+
+// Iniciamos el servidor.
+server.iniciar();
--- principiantes/server.js
+++ principiantes/server.js
@@ -2,26 +2,31 @@
 // través de la variable http.
 var http = require("http");
 
-// Creamos la funcion que se encarga de procesar las peticiones que reciba el
-// servidor.
-function onRequest(request, response) {
-  // Escribimos en la cabecera del response el código 200 indicando que la
-  // petición se ha procesado con écito.
-  response.writeHead(200, {"Content-Type": "text/html"});
-  // Escribimos el cuerpo de la respuesta.
-  response.write("Hola Mundo");
-  // Finalizamos y retornamos la respuesta.
-  response.end();
-  // Escuchamos en el puerto 8888
+// Funcion que se encarga de inicializar el servidor.
+function iniciar () {
+  // Creamos la funcion que se encarga de procesar las peticiones que reciba el
+  // servidor.
+  function onRequest(request, response) {
+    console.log("Petición Recibida.");
+    // Escribimos en la cabecera del response el código 200 indicando que la
+    // petición se ha procesado con écito.
+    response.writeHead(200, {"Content-Type": "text/html"});
+    // Escribimos el cuerpo de la respuesta.
+    response.write("Hola Mundo");
+    // Finalizamos y retornamos la respuesta.
+    response.end();
+    // Escuchamos en el puerto 8888
+  }
+  // Llamamos a una de las funciones que el módulo http ofrece: createServer. Esta
+  // función retorna un objeto, y este objeto tiene un método llamado listen
+  // (escucha), y toma un valor numérico que indica el número de puerto en que
+  // nuestro servidor HTTP va a escuchar.
+  // En nuestro caso el servidor se inicia en el puerto 8888
+  http.createServer(onRequest).listen(8888);
+  // Indicamos que el servidor se ha iniciado correctamente.
+  console.log("Servidor Iniciado.");
 }
 
-// Llamamos a una de las funciones que el módulo http ofrece: createServer. Esta
-// función retorna un objeto, y este objeto tiene un método llamado listen
-// (escucha), y toma un valor numérico que indica el número de puerto en que
-// nuestro servidor HTTP va a escuchar.
-// En nuestro caso el servidor se inicia en el puerto 8888
-http.createServer(onRequest).listen(8888);
-
-// Con esto podemos ver que después de registrar el servidor, la ejecución
-// continua. 
-console.log("Servidor Iniciado.");
+// Exportamos la funcion iniciar que se encarga de iniciar y configurar el
+// servidor.
+exports.iniciar = iniciar;
