1bae07ca3f98a9c9fe7753860d65d9a5e5c2bbb8
Adrian Novegil <adrian.novegil@adesis.com>
2015-11-24 22:00:00 -0600

    Modificación del código del servidor para modificar la definición del código.

--- principiantes/server.js
+++ principiantes/server.js
@@ -2,12 +2,9 @@
 // través de la variable http.
 var http = require("http");
 
-// Llamamos a una de las funciones que el módulo http ofrece: createServer. Esta
-// función retorna un objeto, y este objeto tiene un método llamado listen
-// (escucha), y toma un valor numérico que indica el número de puerto en que
-// nuestro servidor HTTP va a escuchar.
-// En nuestro caso el servidor se inicia en el puerto 8888
-http.createServer(function(request, response) {
+// Creamos la funcion que se encarga de procesar las peticiones que reciba el
+// servidor.
+function onRequest(request, response) {
   // Escribimos en la cabecera del response el código 200 indicando que la
   // petición se ha procesado con écito.
   response.writeHead(200, {"Content-Type": "text/html"});
@@ -16,4 +13,11 @@ http.createServer(function(request, response) {
   // Finalizamos y retornamos la respuesta.
   response.end();
   // Escuchamos en el puerto 8888
-}).listen(8888);
+}
+
+// Llamamos a una de las funciones que el módulo http ofrece: createServer. Esta
+// función retorna un objeto, y este objeto tiene un método llamado listen
+// (escucha), y toma un valor numérico que indica el número de puerto en que
+// nuestro servidor HTTP va a escuchar.
+// En nuestro caso el servidor se inicia en el puerto 8888
+http.createServer(onRequest).listen(8888);
